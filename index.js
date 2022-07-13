63f62ff747b5b0976175b2f71b80f37186541a6f
mmontecalvo <mati.montecalvo@gmail.com>
2022-07-12 23:18:58 -0300

    Cambio de paleta de colores. Se agregó la sección ABOUT ME.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const moon = document.querySelector('.dark');
 const sun = document.querySelector('.light');
+const back = document.querySelector('body');
+
 
 sun.addEventListener('click', () => themeMode('light'));
 moon.addEventListener('click', () => themeMode('dark'));
@@ -8,10 +10,12 @@ const themeMode = (themeState) => {
     if(themeState === 'light'){
         sun.style.display = 'none';
         moon.style.display = 'block';
+        back.style.backgroundColor = '#FFFFFF';
     }
     else if(themeState === 'dark'){
         sun.style.display = 'block';
         moon.style.display = 'none';
+        back.style.backgroundColor = '#000000';
     }
 }
 
@@ -37,3 +41,117 @@ const myFunc = (navCondition) => {
             btnBars.style.display = "block";
     }
 }
+
+
+
+
+
+
+
+// dots is an array of Dot objects,
+// mouse is an object used to track the X and Y position
+   // of the mouse, set with a mousemove event listener below
+   var dots = [],
+   mouse = {
+     x: 0,
+     y: 0
+   };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+ this.x = 0;
+ this.y = 0;
+ this.node = (function(){
+   var n = document.createElement("div");
+   n.className = "trail";
+   document.body.appendChild(n);
+   return n;
+ }());
+};
+// The Dot.prototype.draw() method sets the position of 
+ // the object's <div> node
+Dot.prototype.draw = function() {
+ this.node.style.left = this.x + "px";
+ this.node.style.top = this.y + "px";
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 10; i++) {
+ var d = new Dot();
+ dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+ // Make sure the mouse position is set everytime
+   // draw() is called.
+ var x = mouse.x,
+     y = mouse.y;
+ 
+ // This loop is where all the 90s magic happens
+ dots.forEach(function(dot, index, dots) {
+   var nextDot = dots[index + 1] || dots[0];
+   
+   dot.x = x;
+   dot.y = y;
+   dot.draw();
+   x += (nextDot.x - dot.x) * .5;
+   y += (nextDot.y - dot.y) * .5;
+
+ });
+}
+
+addEventListener("mousemove", function(event) {
+ //event.preventDefault();
+ mouse.x = event.pageX;
+ mouse.y = event.pageY;
+});
+
+// animate() calls draw() then recursively calls itself
+ // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+ draw();
+ requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const text = document.querySelector(".banner__title");
+const box = document.querySelector(".panel-text");
+const img = document.querySelector(".panel-image");
+
+// Hero Parallax
+const tl = gsap.timeline({
+  defaults: { ease: "none", transformOrigin: "50% 50%" },
+  scrollTrigger: {
+    trigger: ".main-content__banner",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
+});
+
+gsap.utils.toArray(".banner__img").forEach((layer) => {
+  const depth = layer.dataset.depth;
+  const movement = -(layer.offsetHeight * depth);
+  tl.to(layer, { y: -movement }, 0);
+});
+
+tl.to(
+  text,
+  {
+    y: -text.offsetHeight * text.dataset.depth,
+    autoAlpha: 0,
+    scale: 1.08,
+    duration: 0.2
+  },
+  0
+)
